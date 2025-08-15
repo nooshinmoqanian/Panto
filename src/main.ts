82246b97f82d916c67ec0f4b3fc780cb91b1b9a2
nooshinmoqanian <nooshinmoqanian@gmail.com>
2025-08-15 09:34:20 +0330

    fix : add exam .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AllExceptionsFilter } from "./common/filters/all-exceptions.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,17 +11,17 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionsFilter());
 
   const config = new DocumentBuilder()
-    .setTitle('PANTOhealth Signals API')
-    .setDescription('IoT X-ray signals processing with RabbitMQ + Mongo')
-    .setVersion('1.0.0')
+    .setTitle("PANTOhealth Signals API")
+    .setDescription("IoT X-ray signals processing with RabbitMQ + Mongo")
+    .setVersion("1.0.0")
     .build();
 
   const doc = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/docs', app, doc);
+  SwaggerModule.setup("/docs", app, doc);
 
   await app.listen(3000);
   console.log(
-    'API: http://localhost:3000  |  Docs: http://localhost:3000/docs',
+    "API: http://localhost:3000  |  Docs: http://localhost:3000/docs",
   );
 }
 bootstrap();
